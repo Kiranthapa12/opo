91b91b7f708a8043800a1998d098e86f2f4ae2e0
Kiran Thapa <108472726+Kiranthapa12@users.noreply.github.com>
2022-07-24 08:45:12 +0545

    Initial commit

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { computed } from 'vue'
+import BaseLevel from '@/components/BaseLevel.vue'
+import JustboilLogo from '@/components/JustboilLogo.vue'
+
+const year = computed(() => new Date().getFullYear())
+</script>
+
+<template>
+  <footer
+    class="bg-white py-2 px-6 dark:bg-gray-900/70"
+  >
+    <BaseLevel>
+      <div class="text-center md:text-left">
+        <b>&copy; {{ year }}, <a
+          href="https://justboil.me/"
+          target="_blank"
+        >JustBoil.me</a>.</b>
+        Get more with <a
+          href="https://tailwind-vue.justboil.me/"
+          target="_blank"
+          class="text-blue-600"
+        >Premium version</a>
+      </div>
+      <div class="md:py-2">
+        <a href="https://justboil.me">
+          <JustboilLogo class="w-auto h-8 md:h-6" />
+        </a>
+      </div>
+    </BaseLevel>
+  </footer>
+</template>
